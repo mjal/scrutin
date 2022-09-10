2f55675f0d5be51be08924dcd7c9dff9e38b268f
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-09-10 11:22:48 +0200

    fix bug and indent

--- src/state.js
+++ src/state.js
@@ -1,32 +1,30 @@
 import { useReducer } from 'react'
 
 const initialState = {
-	electionName: "Élection sans nom",
-	voters: [],
-	candidates: []
+  electionName: "Élection sans nom",
+  voters: [],
+  candidates: []
 }
 
 function reducer(state, action) {
-	console.log(action, state)
   switch (action.type) {
     case 'electionName':
       return {...state, electionName: action.value}
     case 'addVoter':
       return {...state, voters: [...new Set([...state.voters, action.voter])]}
     case 'removeVoter':
-			const index = state.voters.indexOf(action.value)
-			if (index > -1) {
-				state.voters.splice(index, 1)
-			}
-      return {...state, voters: state.value}
+      const index = state.voters.indexOf(action.value)
+      if (index > -1) {
+        state.voters.splice(index, 1)
+      }
+      return {...state, voters: state.voters}
     case 'addCandidate':
       return {...state, candidates: [...new Set([...state.candidates, action.value])]}
     default:
       throw new Error()
   }
-	console.log(state)
 }
 
 export default function() {
-	return useReducer(reducer, initialState);
+  return useReducer(reducer, initialState);
 }
