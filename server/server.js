926aa0778a47242bce7cea9b71917a7999224577
Maxime Lalisse <maxime.lalisse@gmail.com>
2023-02-24 12:13:40 +0545

    feat: send email

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const { Sequelize } = require('sequelize')
 const sjcl = require('sjcl-with-all')
 const cors = require('cors') // TODO: Configure cors
 require('dotenv').config()
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const app = express()
 app.use(express.json())
@@ -33,19 +35,53 @@ app.post('/events', (req, res) => {
   res.json(event.toJSON())
 })
 
-app.post('/users', cors(), (req, res) => {
+app.post('/users', async (req, res, next) => {
   const fullName = req.body.fullName
   const email = req.body.email
   const randomWords = sjcl.random.randomWords(8)
   const secret = sjcl.codec.base32.fromBits(randomWords)
   const emailConfirmationToken = sjcl.codec.base32.fromBits(sjcl.hash.sha256.hash(randomWords))
-  const user = User.create({
+
+  const user = await User.create({
     fullName,
     email,
     emailConfirmed: false,
     emailConfirmationToken
   })
+
+  const msg = {
+    to: email,
+    from: 'hello@scrutin.app',
+    subject: 'Votre inscription sur scrutin.app',
+    text: `Veuillez confirmer votre identité en cliquant ici :
+      ${process.env.CLIENT_URL}/users/confirm_email/${emailConfirmationToken}
+    `,
+    //html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode)
+      console.log(response[0].headers)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
   res.json(user.toJSON())
+})
+
+app.get('/users/confirm_email/:emailConfirmationToken', async (req, res) => {
+
+  const user = await User.findOne({ where: { emailConfirmationToken } });
+  if (user === null) {
+    console.log('Not found!');
+  } else {
+    console.log(user instanceof User);
+    console.log(user.fullName);
+  }
+
 })
 
 const port = process.env.PORT || 8080
