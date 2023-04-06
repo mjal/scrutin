2d311d4da28c36b6fa19410590d5848d17cb8913
Maxime Lalisse <maxime.lalisse@gmail.com>
2023-04-06 16:10:54 +0000

    feat: i18next browser language detector

--- i18n.js
+++ i18n.js
@@ -1,16 +1,22 @@
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import * as ReactNative from "react-native";
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 import en from './i18n/en.json'
 import fr from './i18n/fr.json'
 
-i18next
-  .use(initReactI18next)
-  .init({
-    debug: true,
-    fallbackLng: 'en',
-    resources: {
-      en,
-      fr
-    }
-  })
+let i18nextPipeline = i18next.use(initReactI18next)
+
+if (ReactNative.Platform.OS === "web") {
+  i18nextPipeline = i18nextPipeline.use(LanguageDetector)
+}
+
+i18nextPipeline.init({
+  debug: true,
+  fallbackLng: 'en',
+  resources: {
+    en,
+    fr
+  }
+})
