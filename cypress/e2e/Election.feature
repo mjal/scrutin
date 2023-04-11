6d13c5890d918533ba053248d651692275750834
Maxime Lalisse <maxime.lalisse@gmail.com>
2023-04-11 21:37:58 +0545

    feat: Election creation test

--- cypress/e2e/Election.feature
+++ cypress/e2e/Election.feature
@@ -1,5 +1,21 @@
 Feature: Organize elections
 
+  @focus
   Scenario: Creation
     When I go to new election
-    Then I should see 'You need at least 2 choices'
+    And I add a title
+    And I add choices
+    And I click create
+    Then Election should be created
+
+  Scenario: Adding a voter
+    Given I created an election
+    When I add a user by email
+    Then That user should be able to login and vote
+
+  Scenario: Adding a voter
+    Given I created an election
+    And I invited Mario and Luigi
+    And Mario and Luigi have voted
+    When I compute the election's result
+    Then The election's result is displayed
--- cypress/e2e/Election.js
+++ cypress/e2e/Election.js
@@ -1,10 +1,34 @@
-import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When("I go to new election", () => {
   cy.visit('http://localhost:19006/')
   cy.contains('Create a new election').click()
 })
 
-Then("I should see 'You need at least 2 choices'", () => {
-  cy.contains("You need at least 2 choices")
+When("I add a title", () => {
+  cy.get('[data-testid="election-name"]').type(`Testing election from cypress`, {delay: 0})
 })
+
+When("I add choices", () => {
+  let choices = [
+    "First choice",
+    "Second choice",
+    "Third choice"
+  ]
+
+  for (let choice of choices) {
+    cy.get('[data-testid="choice-list"]').contains('Add').click()
+    cy.get('[data-testid="choice-name"]').type(choice, {delay: 0})
+    cy.get('[data-testid="choice-modal"]').contains('Add').click()
+  }
+})
+
+When("I click create", () => {
+  cy.contains("Create").click()
+})
+
+Then("Election should be created", () => {
+  cy.contains("Testing election from cypress")
+})
+
+
